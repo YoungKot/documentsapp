6ee7206f84d1d6b82003bc75bf1ccc9adfc3ff29
Daniils Korolenko <internalpainy@gmail.com>
2019-11-15 14:41:00 +0200

    Document creation added

--- app/controllers/documents_controller.rb
+++ app/controllers/documents_controller.rb
@@ -1,3 +1,22 @@
 class DocumentsController < ApplicationController
     before_action :authenticate_user!
+
+    def index
+        @documents = Document.all
+    end
+
+    def new
+        @document = Document.new
+    end
+
+    def create
+        @document = Document.new(document_params)
+        @document.save
+        redirect_to documents_path
+    end
+
+    private
+    def document_params
+        params.require(:document).permit(:title, :description)
+    end
 end
