6a277f945ca2c1a73fa45e98581d884be20c39f5
Daniils Korolenko <internalpainy@gmail.com>
2019-11-16 16:24:23 +0200

    attachments can be uploaded

--- app/controllers/documents_controller.rb
+++ app/controllers/documents_controller.rb
@@ -1,47 +1,56 @@
+# frozen_string_literal: true
+
 class DocumentsController < ApplicationController
-    before_action :authenticate_user!
+  before_action :authenticate_user!
 
-    def index
-        @documents = current_user.documents
-    end
+  def index
+    @documents = current_user.documents
+  end
 
-    def new
-        @document = Document.new
-    end
+  def new
+    @document = Document.new
+  end
 
-    def create
-        @document = Document.new(document_params)
-        @document.user_id = current_user.id
-        @document.save
-        redirect_to documents_path
+  def create
+    @document = Document.new(document_params)
+    @document.user_id = current_user.id
+    if @document.save
+      flash[:notice] = 'Document was created!'
+      redirect_to documents_path
+    else
+      render 'new'
     end
-    
-    def show
-        @document = Document.find(params[:id])
-    end
+  end
 
-    def edit
-        @document = Document.find(params[:id])
-    end
+  def show
+    @document = Document.find(params[:id])
+  end
 
-    def update
-        @document = Document.find(params[:id])
-        if @document.update(document_params)
-            redirect_to @document
-         else
-            render 'edit'
-        end
-    end
+  def edit
+    @document = Document.find(params[:id])
+  end
 
-    def destroy
-        @document = Document.find(params[:id])
-        @document.destroy
-     
-        redirect_to documents_path
+  def update
+    @document = Document.find(params[:id])
+    if @document.update(document_params)
+      flash[:notice] = 'Document was updated!'
+      redirect_to @document
+    else
+      render 'edit'
     end
+  end
 
-    private
-    def document_params
-        params.require(:document).permit(:title, :description)
+  def destroy
+    @document = Document.find(params[:id])
+    if @document.destroy
+      flash[:notice] = 'Document was deleted!'
+      redirect_to documents_path
     end
+  end
+
+  private
+
+  def document_params
+    params.require(:document).permit(:title, :description, :attachment)
+  end
 end
---
+++ db/migrate/20191116122925_add_attachment_to_documents.rb
@@ -0,0 +1,7 @@
+# frozen_string_literal: true
+
+class AddAttachmentToDocuments < ActiveRecord::Migration[6.0]
+  def change
+    add_column :documents, :attachment, :string
+  end
+end
